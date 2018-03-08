d287022854c5b7cacd7bbf54962b3f8bcd49004d
dreamingrainbow <michaeladennis@yahoo.com>
2018-03-08 13:46:58 -0700

    Create BallotsController.php
    Adding the skeleton logic for the basic pages.

---
+++ Modules/Ballots/Controllers/BallotsController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Modules\HelloWorld\Controllers;
+use \Library\Controller;
+class Ballots extends Controller
+{
+    public function search(){}
+    public function add(){}
+    public function cast(){}
+    public function results(){}
+}
