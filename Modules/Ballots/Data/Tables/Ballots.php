7cdffa48dcbb0ff7e1e7eb48a92c68b9eca8b472
dreamingrainbow <michaeladennis@yahoo.com>
2018-03-09 16:16:35 -0700

    Update Ballots.php

--- Modules/Ballots/Data/Tables/Ballots.php
+++ Modules/Ballots/Data/Tables/Ballots.php
@@ -58,7 +58,7 @@ class Ballots extends Table
         $sql = 'SELECT Ballots.name, count(Votes.yea) as YeaCount FROM Votes';	
         $sql .= ' INNER JOIN Ballots on (Votes.ballot_id = Ballots.id)';
         $sql .= 'WHERE Ballots.id = ?';
-        $sql . = ' and Votes.yea = "true"';       
+        $sql . = " and Votes.yea = 'true'";       
         $sth = $this->connection()->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);	
         $sth->execute([$id]);	
         return $sth->fetchAll(PDO::FETCH_ASSOC);
@@ -68,7 +68,7 @@ class Ballots extends Table
         $sql = 'SELECT Ballots.name, count(Votes.yea) as YeaCount FROM Votes';	
         $sql .= ' INNER JOIN Ballots on (Votes.ballot_id = Ballots.id)';
         $sql .= 'WHERE Ballots.id = ?';
-        $sql . = ' and Votes.nea = "true"';       
+        $sql . = " and Votes.yea = 'true'";       
         $sth = $this->connection()->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);	
         $sth->execute([$id]);
         return $sth->fetchAll(PDO::FETCH_ASSOC);
@@ -78,7 +78,7 @@ class Ballots extends Table
         $sql = 'SELECT Ballots.name, count(Votes.yea) as YeaCount FROM Votes';	
         $sql .= ' INNER JOIN Ballots on (Votes.ballot_id = Ballots.id)';
         $sql .= 'WHERE Ballots.id = ?';
-        $sql . = ' and Votes.yea = "true"';       
+        $sql . = " and Votes.yea = 'true'";       
         $sth = $this->connection()->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);	
         $sth->execute([$id]);	
         return $sth->fetchAll(PDO::FETCH_ASSOC);
