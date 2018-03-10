b6aad7cc1716599f3eac3053114fd0ab577171f4
dreamingrainbow <michaeladennis@yahoo.com>
2018-03-09 23:24:57 -0700

    Update Pages.php

--- Modules/Primary/Controllers/Pages.php
+++ Modules/Primary/Controllers/Pages.php
@@ -5,8 +5,33 @@ class Pages extends Controller
 {
     public function home()
     {
-
+        if(isset($_REQUEST['limit']) && $_REQUEST['limit']) {
+            if(preg_match ('/^[0-9]+$/', $_REQUEST['limit'])) {
+                $limit = $_REQUEST['limit'];
+            } else {
+                $limit = 10;        
+            }
+        } else {
+            $limit = 10;
+        }
         
+        if(isset($_REQUEST['offset']) && $_REQUEST['offset']) {
+            if(preg_match ('/^[0-9]+$/', $_REQUEST['offset'])) {
+                $offset = $_REQUEST['offset'];
+            } else {
+                $offset = 10;        
+            }
+        } else {
+            $offset = 0;
+        }
+        
+        if(isset($_REQUEST['output']) && $_REQUEST['output'] === 'json') {
+            $this->setNoRenderView();
+            header('Content-Type: application/json');
+            echo json_encode($this->getTable('Ballots','Ballots')->getBallots($limit, $offset));
+        } else {
+            $this->ballots = $this->getTable('Ballots','Ballots')->getBallots($limit, $offset);
+        }
     }
 }
 
