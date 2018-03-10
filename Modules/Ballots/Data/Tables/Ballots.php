ab8ca817b02b21b5a8a9547238b7ba59a42c01b1
dreamingrainbow <michaeladennis@yahoo.com>
2018-03-10 01:38:09 -0700

    Update Ballots.php

--- Modules/Ballots/Data/Tables/Ballots.php
+++ Modules/Ballots/Data/Tables/Ballots.php
@@ -69,12 +69,11 @@ class Ballots extends Table
         $sql .= "FROM Votes";
         $sql .= ' LEFT JOIN Ballots on (Votes.ballot_id = Ballots.id)';
         $sql .= ' WHERE Ballots.id = ?';  
-        debug($sql);
         $sth = $this->connection()->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-        $sth->bindParam(1, (int)$id, PDO::PARAM_INT);
-        $sth->bindParam(2, (int)$id, PDO::PARAM_INT);
-        $sth->bindParam(3, (int)$id, PDO::PARAM_INT);
-        $sth->bindParam(4, (int)$id, PDO::PARAM_INT);
+        $sth->bindParam(1, $id, PDO::PARAM_INT);
+        $sth->bindParam(2, $id, PDO::PARAM_INT);
+        $sth->bindParam(3, $id, PDO::PARAM_INT);
+        $sth->bindParam(4, $id, PDO::PARAM_INT);
         $sth->execute();	
         return $sth->fetchAll(PDO::FETCH_ASSOC);
     }
