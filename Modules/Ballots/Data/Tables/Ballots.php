dfa918220e7df896e7eba6cce64a5c7ed1bb277e
dreamingrainbow <michaeladennis@yahoo.com>
2018-03-10 01:36:51 -0700

    Update Ballots.php

--- Modules/Ballots/Data/Tables/Ballots.php
+++ Modules/Ballots/Data/Tables/Ballots.php
@@ -71,10 +71,10 @@ class Ballots extends Table
         $sql .= ' WHERE Ballots.id = ?';  
         debug($sql);
         $sth = $this->connection()->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-        $sth->bindParam(1, $id, PDO::PARAM_INT);
-        $sth->bindParam(2, $id, PDO::PARAM_INT);
-        $sth->bindParam(3, $id, PDO::PARAM_INT);
-        $sth->bindParam(4, $id, PDO::PARAM_INT);
+        $sth->bindParam(1, (int)$id, PDO::PARAM_INT);
+        $sth->bindParam(2, (int)$id, PDO::PARAM_INT);
+        $sth->bindParam(3, (int)$id, PDO::PARAM_INT);
+        $sth->bindParam(4, (int)$id, PDO::PARAM_INT);
         $sth->execute();	
         return $sth->fetchAll(PDO::FETCH_ASSOC);
     }
